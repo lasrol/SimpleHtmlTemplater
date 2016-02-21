ce5b7e220984a21efce444097d53ee1ca09292cc
Lasse Rolstad <lasrol88@gmail.com>
2016-02-21 23:28:19 +0100

    Moved patch script to solution level and loop trough src projects to patch.

---
+++ Patch-ProjectJson.ps1
@@ -0,0 +1,9 @@
+﻿Param([string]$Version)
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+Get-ChildItem -Path "$scriptDir\src\" | where {$_.PSIsContainer} | foreach {
+    $path = $_.FullName+"\project.json"
+    $json = Get-Content -Raw -Path $path | ConvertFrom-Json
+    $json.version = $Version
+    $json | ConvertTo-Json -depth 999 | Out-File $path  
+}
--- src/SimpleHtmlTemplater/Patch-ProjectJson.ps1
+++
@@ -1,7 +0,0 @@
-﻿Param([string]$Version)
-
-$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$filePath = $scriptDir+"\Project.json"
-$json = Get-Content -Raw -Path $filePath | ConvertFrom-Json
-$json.version = $Version
-$json | ConvertTo-Json -depth 999 | Out-File $filePath
